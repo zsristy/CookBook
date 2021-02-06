db4d87d7b3182ee47c95e08c47709cc60d713cf7
Maliha Noushin Raida <malihanoushin@iut-dhaka.edu>
2021-02-06 12:41:27 +0600

    logout added

--- cookbook/src/component/Dashboard.js
+++ cookbook/src/component/Dashboard.js
@@ -1,5 +1,25 @@
-import React from "react";
+import React,{useState} from "react";
+import { Card, Button, Container } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const { currentUser, logout } = useAuth();
+  const history = useHistory();
+  const handleLogout = async () => {
+    try {
+      await logout().then(() => {
+        history.push("/login");
+      });
+    } catch (error) {
+      setError(error);
+    }
+  };
+  return (
+    <div>
+      <Button variant="Link" onClick={handleLogout}>
+        Log out
+      </Button>
+    </div>
+  );
 }
