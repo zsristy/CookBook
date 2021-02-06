72b935cf4b45d74235b161ec9c7f9401ebbb6a77
Maliha Noushin Raida <malihanoushin@iut-dhaka.edu>
2021-02-06 14:52:45 +0600

    Dashboard and dashboard header created

--- cookbook/src/component/Dashboard.js
+++ cookbook/src/component/Dashboard.js
@@ -1,7 +1,9 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { Card, Button, Container } from "react-bootstrap";
 import { Link, useHistory } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import DashboardHeader from "./DashboardHeader";
+import dashboad_back from "../images/dashboad_back.jpg";
 
 export default function Dashboard() {
   const { currentUser, logout } = useAuth();
@@ -17,9 +19,22 @@ export default function Dashboard() {
   };
   return (
     <div>
-      <Button variant="Link" onClick={handleLogout}>
-        Log out
-      </Button>
+      <div
+        style={{
+          backgroundSize: "cover",
+          position: 'fixed',
+          height:'100%',
+          width:'100%',
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundImage: "url(" + dashboad_back + ")",
+        }}
+      >
+        <DashboardHeader />
+        <Button variant="Link" onClick={handleLogout}>
+          Log out
+        </Button>
+      </div>
     </div>
   );
 }
---
+++ cookbook/src/component/DashboardHeader.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Navbar } from "react-bootstrap";
+
+export default function DashboardHeader() {
+  return <Navbar style={{ backgroundColor: "#fff00" }}></Navbar>;
+}
