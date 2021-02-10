9606aec19c8b832670bb905abec10ef7f0d1b7e4
Maliha Noushin Raida <malihanoushin@iut-dhaka.edu>
2021-02-10 22:46:31 +0600

    get recipe firebase

--- cookbook/src/firebase/getRecipe.js
+++ cookbook/src/firebase/getRecipe.js
@@ -1,16 +1,20 @@
 import app from "./Firebase";
+import "firebase/firestore";
 
-const getRecipe = async (setRecipeList) => {
-  await app
-    .firestore()
-    .collection("recipe")
-    .get()
-    .then((item) => {
-      console.log(item);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+const getRecipe = async (searchTitle, setRecipeList) => {
+  const snapshot = await app.firestore().collection("recipe").get();
+  if (snapshot.empty) {
+    console.log("No matching documents.");
+    return;
+  }
+  const recipeList = {};
+  snapshot.forEach((doc) => {
+    const RecipeLable = doc.data().recipe.label.toLowerCase();
+    if (RecipeLable.includes(searchTitle)) {
+      recipeList[doc.id] = doc.data();
+    }
+  });
+  setRecipeList(recipeList);
 };
 
 export default getRecipe;
