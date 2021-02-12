1d363e0e950eff97c2d58468ce30196eb8bead1c
Maliha Noushin Raida <malihanoushin@iut-dhaka.edu>
2021-02-12 12:35:06 +0600

    filter added to getFIlterRecipe search

--- cookbook/src/firebase/getFilterRecipe.js
+++ cookbook/src/firebase/getFilterRecipe.js
@@ -16,8 +16,20 @@ const getFilterRecipe = async (
   }
   const recipeList = [];
   snapshot.forEach((doc) => {
+    const dietLabels = doc.data().recipe.dietLabels;
+    const healthLabels = doc.data().recipe.healthLabels;
+    const mealLabel = doc.data().recipe.meal ? doc.data().recipe.meal : "";
+    const cuisineLabel = doc.data().recipe.cuisine
+      ? doc.data().recipe.cuisine
+      : "";
     const RecipeLable = doc.data().recipe.label.toLowerCase();
-    if (RecipeLable.includes(title)) {
+    if (
+      RecipeLable.includes(title) &
+      diet.every((val) => dietLabels.includes(val)) &
+      health.every((val) => healthLabels.includes(val)) &
+      (meal === mealLabel) &
+      (cuisine === cuisineLabel)
+    ) {
       const data = doc.data();
       data["recipeId"] = doc.id;
       recipeList.push(data);
