5c8ee8be91e7db2ebd46fa6b65db0bc5d5e6ea1c
Maliha Noushin Raida <malihanoushin@iut-dhaka.edu>
2021-02-13 06:06:24 +0600

    addrecipe input handled

--- cookbook/src/firebase/addRecipe.js
+++ cookbook/src/firebase/addRecipe.js
@@ -1,17 +1,39 @@
 import app from "./Firebase";
 import "firebase/firestore";
 
-const addRecipe = async (recipe) => {
+const addRecipe = async (
+  title,
+  meal,
+  cuisine,
+  diet,
+  health,
+  validItem,
+  imageUrl,
+  recipePrep,
+  author
+) => {
   await app
     .firestore()
     .collection("recipe")
-    .add(recipe)
-    .then(() => {
-      return true;
+    .add({
+      bookmarked: false,
+      bought: false,
+      recipe: {
+        dietLabels: diet,
+        healthLabels: health,
+        image: imageUrl,
+        ingredientLines: validItem,
+        meal: meal,
+        cuisine: cuisine,
+        label: title,
+        author: author,
+        prep: recipePrep,
+      },
     })
+    .then(() => {})
     .catch((error) => {
       console.log(error);
-      return false;
+      alert(error);
     });
 };
 
